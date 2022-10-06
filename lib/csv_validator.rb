38da27761486b09373097eabf5430c001b54c88b
陳建良 <chenjianliang@chenjiaangdeAir.lan>
2022-10-06 10:00:02 +0800

    pass check null and timestamp in csv validator

--- lib/csv_validator.rb
+++ lib/csv_validator.rb
@@ -9,14 +9,19 @@ class CsvValidator
     @table_info = table_info
     @csv = CSV.table(file_path, { header_converters: lambda { |header| header.to_s } })
     @errors = []
+    @not_null_col = @table_info.not_null_columns
   end
 
   def valid?
     check_empty(@csv)
     check_duplicate_id(@csv)
+    check_not_null(@csv)
+    check_time(@csv)
+
+
     @errors.empty?
   end
-  
+
   private
   def check_empty(data)
     @errors << "Empty Content" if data.empty?
@@ -28,6 +33,20 @@ class CsvValidator
       true
     else
       @errors << "Duplicate Ids: #{dup_id}"
+    end
+  end
+  def check_not_null(data)
+    @table_info.not_null_columns.each do |col|
+      data[col].each_with_index do |value, index|
+        @errors << "Not Null Violation at #{col} in Row ID=#{index + 1}" if value.nil?
+      end
+    end
+  end
+  def check_time(data)
+    @table_info.timestamp_columns.each do |col|
+      data[col].each_with_index do |value, index|
+        @errors <<  "Time Format Violation at #{col} in Row ID=#{index + 1}" if (DateTime.parse(value) rescue ArgumentError) == ArgumentError
+      end
     end
   end
 end
--- lib/table_info.rb
+++ lib/table_info.rb
@@ -5,9 +5,7 @@ require 'active_support/core_ext'
 
 class TableInfo
   attr_reader :localized_header_pattern
-  # def localized_header_pattern
-  #    @localized_header_pattern
-  #  end
+  
   def initialize(schema)
     @schema = schema
     @header_with_limit = []
@@ -24,17 +22,30 @@ class TableInfo
     @schema.select{|_col_name, col_type| 
                   !col_type[:null] && !col_type[:auto_increment] && !col_type[:default] }
                   .keys
-
+    
+    #   @schema.reject { |_col_name, column_val|
+    #   column_val[:null] || column_val[:auto_increment] || column_val[:default].present?
+    # }.keys
   end
 
   def length_limit_data(headers)
-    header_with_language = headers.select { |x| x.match(/\[en|\[zh/) }
-    header_with_language.each do |header|
-      header_without_language = header.split('[')[0]
-      limit = @schema["#{header_without_language}"][:limit] || 255
-      @header_with_limit << [header, limit]
-    end
-    @header_with_limit
+    # header_with_language = headers.select { |x| x.match(/\[en\]|\[zh\]/) }
+    # header_with_language.each do |header|
+    #   header_without_language = header.split('[')[0]
+    #   limit = @schema["#{header_without_language}"][:limit] || 255
+    #   @header_with_limit << [header, limit]
+    # end
+    # @header_with_limit
+
+    multi_lan_headers = headers.select { |header| /[a-zA-Z]+\[[a-z]+\]/.match(header) }
+
+    headers
+      .select { |header| multi_lan_headers.include?(header) }
+      .map { |header|
+        header_only = header.split('[')[0]
+        limit = @schema[header_only][:limit].presence || 255
+       p [header, limit]
+      }
 
   end
 
